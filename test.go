ce5ad613010ed6f70dad86e2e7bce1d7881a4cbe
JokoPrasetyoUtomo <joko345@users.noreply.github.com>
2024-10-25 15:59:44 +0700

    new

--- test.go
+++ test.go
@@ -3,8 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var last string // Declare a variable 'last'
-	last = "new"    // Assign a value to 'last'
+	var last string = "new"
 
-	fmt.Print("halo 2 ", last) // Print "halo new"
+	fmt.Print("halo 2 ", last)
 }
