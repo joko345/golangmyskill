4e204bb6e39ff6db00d0b840b81406fcc713aa11
JokoPrasetyoUtomo <joko345@users.noreply.github.com>
2024-10-25 15:58:31 +0700

    new

---
+++ test.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var last string // Declare a variable 'last'
+	last = "new"    // Assign a value to 'last'
+
+	fmt.Print("halo 2 ", last) // Print "halo new"
+}
