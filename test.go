f72d80bbcd3a56e110c45e51072942397f5714fd
JokoPrasetyoUtomo <joko345@users.noreply.github.com>
2024-10-26 19:57:01 +0700

    new

--- test.go
+++ test.go
@@ -1,9 +1,19 @@
 package main
 
+/* */
+
 import "fmt"
 
 func main() {
-	var last string = "new"
+	// var last string = "new"
+	// const valid = "main"
+	var positif int = 89
+	var negatif = -123
+	var decima = 2.2
+	var test *int = &positif //&positif dengan amperstand & akan menampilkan alamat memori
+	*test = 2                //dengan asterisk maka var positif valuenya akan ikut berubah
 
-	fmt.Print("halo 2 ", last)
+	// fmt.Print("halo 2 ", last)
+	fmt.Println("positif = ", positif, decima, *test) //agar muncul value pakai asterix *
+	fmt.Print("negatif = ", negatif)
 }
