a8e27857526d2e4759cedf2e450ae2b55e221cf3
JokoPrasetyoUtomo <joko345@users.noreply.github.com>
2024-10-27 19:53:10 +0700

    new

--- relasi/relasi.go
+++ relasi/relasi.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	var point = 8
-	if point == 10 {
+	if point == 10 && point < 20 {
 		fmt.Println("lulus sempurna")
 	} else if point > 5 {
 		fmt.Println("lulus")
@@ -12,6 +12,16 @@ func main() {
 		fmt.Println("kurang")
 	} else {
 		fmt.Println("tidak lulus")
+	}
+
+	var tukar = 1
+	switch tukar {
+	case 8:
+		fmt.Println("lulus sempurna")
+	case 5:
+		fmt.Println("tidak lulus")
+	default:
+		fmt.Println("kurang")
 	}
 	var equal = (2 == 2)
 	fmt.Print("nilai \n", equal)
